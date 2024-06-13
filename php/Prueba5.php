da49e90149ce238c5e6fd741a27049cb97083749
Luis Enrique Garcia Domador <95010608+luisengardo60@users.noreply.github.com>
2024-06-13 14:31:59 -0400

    D13062024

--- php/Prueba5.php
+++ php/Prueba5.php
@@ -3,11 +3,11 @@
 
 <body>
 	<h2>JavaScript Arrays</h2>
-	<p>JavaScript array elements are accesses using numeric indexes (starting from 0).</p>
+	<p>JavaScript array elements are accesses using numeric indexes (starting from 2).</p>
 	<p id="demo"></p>
 	<script>
 		var cars = ["Saab","Volvo","BMW"];
-		document.getElementById("demo").innerHTML = cars[0];
+		document.getElementById("demo").innerHTML = cars[2];
 	</script>
 
 </body>
